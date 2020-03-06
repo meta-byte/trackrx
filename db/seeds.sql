fc2566e7b8d5e0b931766f84dc3bc0078ceacdc3
Garrett Howard <hylander.garrett@gmail.com>
2020-03-05 19:49:35 -0700

    created seeds.sql and created database seeds

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO users (firstname, lastname, email)
+VALUES
+("Garrett", "Howard", "hylander.garrett@gmail.com"),
+("Jordan", "Hanson", "jordanmercedes@gmail.com"),
+("Tyler", "Cain", "tecain13@gmail.com");
+
+INSERT INTO medication(userID, medicationName, medicationDosage, medicationQuantity, medicationFrequency)
+VALUES
+(1, "Vicodin", "10mg", 30, "One to Two tablets every four to six hours as needed."),
+(2, "Amoxicillin", "250mg", 21, "500mg ever eight hours."),
+(3, "Flonase", "50mcg", 30, "One or two sprays in each nostril once a day as needed.");
