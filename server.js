7b3794b80c4d7096bc933ac9559f29aafe67218d
Garrett Howard <hylander.garrett@gmail.com>
2020-03-07 11:34:45 -0700

    added routes for server to display pages to client

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // Requiring necessary npm packages
 var express = require("express");
-var session = require("express-session");
+// var session = require("express-session");
 // Requiring passport as we've configured it
-var passport = require("./config/passport");
+// var passport = require("./config/passport");
 
 // Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 8080;
@@ -15,16 +15,16 @@ app.use(express.json());
 app.use(express.static("public"));
 // We need to use sessions to keep track of our user's login status
 // app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 // Requiring our routes
 require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
+// require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(function () {
-    app.listen(PORT, function () {
-        console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
-    });
+// db.sequelize.sync().then(function () {
+app.listen(PORT, function () {
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
 });
+// });
